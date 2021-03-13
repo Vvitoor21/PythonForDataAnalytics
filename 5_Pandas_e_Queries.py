f676bb25599d95cf0860e7093afb1f148b3f8937
Vitor <vitor.duartegot@gmail.com>
2021-03-13 16:52:24 -0300

    Update 5_Pandas_e_Queries.py
    query em pandas

--- 5_Pandas_e_Queries.py
+++ 5_Pandas_e_Queries.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import random as rd
+import matplotlib.pyplot as plt
+
+data = pd.DataFrame(pd.read_csv('C:/Users/vitor/Desktop/Estudos/DataSet.csv'))
+print(data)
+
+print(data.query('Nome=="Vitor"'))
+
+print(data.query('Peso > 60'))
+
+print(data.query('Peso > 60 and Local == "São Paulo"'))
 
